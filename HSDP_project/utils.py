c12410bdfca85517bc5938fb15286dc5344b5b3d
smart1i <martisilver98@gmail.com>
2022-03-21 18:02:08 +0200

    patient data organized

--- HSDP_project/utils.py
+++ HSDP_project/utils.py
@@ -86,3 +86,43 @@ for data in my_data:
         hemodonor_data.append(data)
     else:
         pass
+
+# this is the exact same function as before but simply collects the recods in separate lists according to the 
+# type of record 
+
+def patient_data_organized (patient_id):
+    heartrate_data =[]
+    weight_data =[]
+    bp_data =[]
+    systolic_data =[]
+    diastolic_data =[]
+    oxigen_saturation =[]
+    emoglobin_data =[]
+    the_patient = select_patient(patient_id)
+    all_data = hemodonor_client.get_all_data_for_patient(the_patient.get('id'))
+    for index in all_data:
+            if index['resource'].get('id', {}).endswith('heartrate' ):
+                heartrate_data.append(index)
+            if index['resource'].get('id', {}).endswith('weight' ):
+                weight_data.append(index)
+            if index['resource'].get('id', {}).endswith('bp' ):
+                bp_data.append(index)
+            if index['resource'].get('id', {}).endswith('systolic' ):
+                systolic_data.append(index)
+            if index['resource'].get('id', {}).endswith('diastolic' ):
+                diastolic_data.append(index)
+            if index['resource'].get('id', {}).endswith('oxygen_saturation' ):
+                oxigen_saturation.append(index)
+            if index['resource'].get('id', {}).endswith('lab' ) :
+                lab = index['resource']
+                if lab.get('code', {}).get('text') == 'Hgb Bld-mCnc':         
+                    emoglobin_data.append(index) 
+    relevant_data=[*heartrate_data, *weight_data, *bp_data, *systolic_data, *diastolic_data, *oxigen_saturation, *emoglobin_data]    
+    if len(relevant_data)==0:
+        print('No relevant data for patient {}'.format(patient_id))
+        return 0,0,0,0,0,0,0  #this is just random values I put to make it work even when there's no data to return
+    elif len(heartrate_data)==0 or len(weight_data)==0 or len(bp_data)==0 or len(systolic_data)==0 or len(diastolic_data)==0 or len(oxigen_saturation)==0 or len(emoglobin_data)==0:
+        print( 'Informations missing for patient : {}'.format(patient_id))
+        return 0,0,0,0,0,0,0 #this is just random values I put to make it work even when there's no enough data to conduct the analysis of eligibility of that patient
+    else :
+        return heartrate_data, weight_data, bp_data, systolic_data ,diastolic_data, oxigen_saturation, emoglobin_data 
