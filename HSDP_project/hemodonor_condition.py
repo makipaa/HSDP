2e126359721503b645459188fe97a09bbd12106b
Ousaamah Abidine <Ousaamah@4aeb0f7d-1cc8-4d8d-8d8e-7b28af44dcba.home>
2022-04-16 14:29:32 +0200

    DONOR PAGE UPDATED

--- HSDP_project/hemodonor_condition.py
+++ HSDP_project/hemodonor_condition.py
@@ -6,13 +6,19 @@ def condition(donation_interval, age, weight, blood_pressure, hemoglobin, gender
             if blood_pressure >=50 <= 180:
                 if hemoglobin >= 12.5 <= 20 and gender == 'female':
                     print('You are eligible to donate')
+                    return True
                 elif hemoglobin >= 13 <= 20 and gender == 'male':
                     print('You are eligible to donate')
+                    return True
                 else:
                     print('Not eligible')
+                    return False
             else:
                 print('Not eligible: your blood pressure is not within the acceptable limits 50-180mmHg')
+                return False
         else:
             print('Not eligible: You must weigh above 50kg')
+            return False
     else:
         print('Not eligible: Your last blood donation should be at least 8 weeks ago and you must be atleast 17yo')
+        return False
