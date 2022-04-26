878f663fc1aecf92d8ed99284992e305022d7361
Antti-Jussi Mäkipää <antti-jussi.makipaa@tuni.fi>
2022-04-26 09:33:30 +0300

    Updated FHRI client class structure

--- HSDP_project/fhir_client.py
+++ HSDP_project/fhir_client.py
@@ -1,36 +1,102 @@
-#!/usr/bin/env python2.7
-# -*- coding: utf-8 -*-
-import json
 import requests
-from pprint import pprint
-
-__author__ = "Milla Jauhiainen"
-__copyright__ = "Copyright 2019, Tampere University"
-__email__ = "milla.jauhiainen@tuni.fi"
+from utils import calculate_age
 
 
-class SimpleFHIRClient(object):
-    def __init__(self, server_url, server_user, server_password, debug=False):
-        self.debug = debug
+class hemodonor_FHIR_client(object):
+    def __init__(self, server_url, server_user, server_password):
         self.server_url = server_url
         self.server_user = server_user
         self.server_password = server_password
-
+        self.all_patients = self.get_all_patients()
+        
+    
+   
     def get_all_patients(self):
         requesturl = self.server_url + "/Patient?_format=json"
         entries = self._get_json(requesturl)["entry"]
         return [entry["resource"] for entry in entries]
 
+   
     def get_all_data_for_patient(self, patient_id):
         requesturl = self.server_url + "/Patient/" + \
             patient_id + "$everything?_format=json"
         return self._get_json(requesturl)["entry"]
-
+    
     def _get_json(self, requesturl):
         response = requests.get(requesturl,
                                 auth=(self.server_user, self.server_password))
         response.raise_for_status()
         result = response.json()
-        if self.debug:
-            pprint(result)
         return result
+    
+    # Get relevant data for the patient
+    def get_relevant_data(self, patient_id):
+
+        data = {
+            'weight' : [],
+            'systolic' : [],
+            'diastolic' : [],
+            'oxygen_saturation' : [],
+            'hemoglobin' : [],
+            'gender' : '',
+            'age' : ''
+        }
+        birth_date = ''
+        # Fetch gender and age
+        patients = self.all_patients
+        for patient in patients:
+            if patient.get('id', {}) == patient_id:
+                data['gender'] = patient['gender']
+                birth_date = patient['birthDate']
+
+        age = calculate_age(birth_date)
+        data['age'] = age
+        
+        # Fetch medical data
+        all_data = self.get_all_data_for_patient(patient_id)
+        for index in all_data:
+                resource = index['resource']
+                if resource.get('id', {}).endswith('weight'):
+                    data['weight'].append(resource['valueQuantity']['value'])
+
+                elif resource.get('id', {}).endswith('systolic'):
+                    data['systolic'].append(resource['valueQuantity']['value'])
+
+                elif resource.get('id', {}).endswith('diastolic'):
+                    data['diastolic'].append(resource['valueQuantity']['value'])
+                
+                elif resource.get('id', {}).endswith('oxygen_saturation'):
+                    data['oxygen_saturation'].append(resource['valueQuantity']['value'])
+
+                elif resource.get('id', {}).endswith('lab'):
+                    lab = index['resource']
+                    if lab.get('code', {}).get('text') == 'Hgb Bld-mCnc':         
+                        data['hemoglobin'].append(resource['valueQuantity']['value'])
+    
+        
+        return data
+        
+    def get_latest_measurements(self, patient_id):
+        latest_data = {
+            'weight' : '',
+            'systolic' : '',
+            'diastolic' : '',
+            'oxygen_saturation' : '',
+            'hemoglobin' : ''
+        }
+        all_data = self.get_relevant_data(patient_id)
+        for key, item in all_data.items():
+            # Check if there are any measurements
+            if not item:
+                latest_data[key] = 'No data'
+            elif key == 'gender' or key == 'age':
+                latest_data[key] = item
+            else:
+                latest_data[key] = item[0]
+        return latest_data
+
+hemodonor_client = hemodonor_FHIR_client(
+    server_url="http://tutsgnfhir.com",
+    server_user="tutfhir",
+    server_password="tutfhir1"
+)
--- HSDP_project/hemodonor/views.py
+++ HSDP_project/hemodonor/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
 from django.contrib.auth.models import User
 
-from utils import get_latest_measurements
-from utils import get_relevant_data
 from hemodonor_condition import condition
 from .forms import user_register_form
-from .models import donor_data
-
+from fhir_client import hemodonor_client as h_client
+import datetime
 
 def register(request):
     if request.method == 'POST':
@@ -17,7 +14,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('uname')
             messages.success(request, f'Account created for {username}!')
-            return redirect('home/')
+            return redirect('/home/')
     else:
         form = user_register_form()
     return render(request, 'register.html', {'form':form})
@@ -31,22 +28,24 @@ def home(request):
 
         if request.user.username == 'doctor':
             # Exclude admin and doctor from this list
-            list_of_users = list(User.objects.all()[3:])
+            list_of_users = User.objects.all()[3:]
      
             all_users = []
+            t1 = datetime.datetime.now()
             for user_obj in list_of_users:
-                donor_data = get_latest_measurements(user_obj.username)
+                donor_data = h_client.get_latest_measurements(user_obj.username)
                  # TODO Replace the dummy values
                 #eligbility = condition(10, donor_data['age'], donor_data['weight'], 60, donor_data['hemoglobin'], donor_data['gender'])
                 all_users.append({'data' : user_obj, 'elig' : 0})
-
+            t2 = datetime.datetime.now()
+            print((t2-t1).total_seconds())
             context = {
                 'users' : all_users,
             }
             return render(request, 'doctor_home.html', context)
         else:
-            data = get_relevant_data(request.user.username)
-            latest_measurements = get_latest_measurements(request.user.username)
+            data = h_client.get_relevant_data(request.user.username)
+            latest_measurements = h_client.get_latest_measurements(request.user.username)
             context = {
                 'user_data': data,
                 'latest_measurements' : latest_measurements
@@ -56,8 +55,8 @@ def home(request):
 def doctor_donor(request, donor_id):
     if not request.user.is_superuser:
         return redirect('../../home')
-    data = get_relevant_data(donor_id)
-    latest_measurements = get_latest_measurements(donor_id)
+    data = h_client.get_relevant_data(donor_id)
+    latest_measurements = h_client.get_latest_measurements(donor_id)
     personal_info = User.objects.filter(username=donor_id).first()
     context = {
         'user_data': data,
